630ad0ac1ebd45fc154bc8f76ae73f467d656a5d
yun.wang <wy19910222@hotmail.com>
2024-01-02 20:45:46 +0800

    整理代码

--- Assets/Tools/LuaBehaviour/Scripts/Editor/LuaBehaviourWithPathInspector.cs
+++ Assets/Tools/LuaBehaviour/Scripts/Editor/LuaBehaviourWithPathInspector.cs
@@ -49,60 +49,49 @@ namespace LuaApp {
 
 		protected void DrawLuaPath() {
 			GUILayout.BeginHorizontal();
+			EditorGUI.BeginDisabledGroup(Application.isPlaying);
 
 			GUILayout.Label("Lua Script", GUILayout.Width(80F));
 
 			bool settingsExist = File.Exists("ProjectSettings/LuaBehaviourSettings.asset");
-			if (settingsExist) {
-				// 已经设置过Lua根目录，支持文件拖放
-				EditorGUI.BeginDisabledGroup(Application.isPlaying);
-				string luaPath = LuaBehaviour.luaPath;
-				bool luaPathIsEmpty = string.IsNullOrEmpty(luaPath);
-				string luaSrcPath = LuaBehaviourSettings.instance.luaSrcPath;
-				string luaFileExtension = LuaBehaviourSettings.instance.luaFileExtension;
-				UObject asset = luaPathIsEmpty ? null : AssetDatabase.LoadAssetAtPath<TextAsset>(luaSrcPath + luaPath.Replace(".", "/") + luaFileExtension);
-				if (luaPathIsEmpty || asset) {
-					UObject newAsset = EditorGUILayout.ObjectField(asset, typeof(TextAsset), true);
-					if (newAsset != asset) {
-						// 根据lua文件记录lua路径
-						if (!newAsset) {
-							asset = null;
-							luaPath = string.Empty;
+			string luaPath = LuaBehaviour.luaPath;
+			bool luaPathIsEmpty = string.IsNullOrEmpty(luaPath);
+			string luaSrcPath = LuaBehaviourSettings.instance.luaSrcPath;
+			string luaFileExtension = LuaBehaviourSettings.instance.luaFileExtension;
+			UObject asset = !settingsExist || luaPathIsEmpty ? null : AssetDatabase.LoadAssetAtPath<TextAsset>(luaSrcPath + luaPath.Replace(".", "/") + luaFileExtension);;
+			if (asset) {
+				// 已经设置过Lua根目录且能找到Lua文件，则支持文件拖放
+				UObject newAsset = EditorGUILayout.ObjectField(asset, typeof(TextAsset), true);
+				if (newAsset != asset) {
+					// 根据lua文件记录lua路径
+					if (!newAsset) {
+						asset = null;
+						luaPath = string.Empty;
+						Undo.RecordObject(LuaBehaviour, "LuaPath");
+						LuaBehaviour.luaPath = luaPath;
+					} else {
+						string newLuaPath = AssetDatabase.GetAssetPath(newAsset);
+						if (newLuaPath.StartsWith(luaSrcPath) && newLuaPath.EndsWith(luaFileExtension)) {
+							int length = newLuaPath.Length - luaSrcPath.Length - luaFileExtension.Length;
+							luaPath = newLuaPath.Substring(luaSrcPath.Length, length).Replace("/", ".");
+							asset = newAsset;
 							Undo.RecordObject(LuaBehaviour, "LuaPath");
 							LuaBehaviour.luaPath = luaPath;
-						} else {
-							string newLuaPath = AssetDatabase.GetAssetPath(newAsset);
-							if (newLuaPath.StartsWith(luaSrcPath) && newLuaPath.EndsWith(luaFileExtension)) {
-								int length = newLuaPath.Length - luaSrcPath.Length - luaFileExtension.Length;
-								luaPath = newLuaPath.Substring(luaSrcPath.Length, length).Replace("/", ".");
-								asset = newAsset;
-								Undo.RecordObject(LuaBehaviour, "LuaPath");
-								LuaBehaviour.luaPath = luaPath;
-							}
 						}
 					}
-					// 推断按钮
-					if (asset && GUILayout.Button("Deduce Fields", GUILayout.Width(100F))) {
-						Type type = LuaBehaviour.GetType();
-						FieldInfo field = type.GetField("m_InjectionList", BindingFlags.Instance | BindingFlags.NonPublic);
-						if (field?.GetValue(target) is List<Injection7> injections) {
-							LuaInjectionDataDeduce.DeduceFields(injections, luaPath);
-						}
-					}
-				} else {
-					// 如果找不到lua文件，则显示文本框
-					string newLuaPath = EditorGUILayout.TextField(luaPath);
-					if (newLuaPath != luaPath) {
-						Undo.RecordObject(LuaBehaviour, "LuaPath");
-						LuaBehaviour.luaPath = newLuaPath;
-					}
-					EditorGUILayout.LabelField(EditorGUIUtility.IconContent("CollabConflict Icon"), GUILayout.Width(20F));
 				}
-				EditorGUI.EndDisabledGroup();
+				// 推断按钮
+				if (asset && GUILayout.Button("Deduce Fields", GUILayout.Width(100F))) {
+					Type type = LuaBehaviour.GetType();
+					FieldInfo field = type.GetField("m_InjectionList", BindingFlags.Instance | BindingFlags.NonPublic);
+					if (field?.GetValue(target) is List<Injection7> injections) {
+						LuaInjectionDataDeduce.DeduceFields(injections, luaPath);
+					}
+				}
 			} else {
-				// 尚未设置Lua根目录，显示文本框和设置按钮
-				string newLuaPath = EditorGUILayout.TextField(LuaBehaviour.luaPath);
-				if (newLuaPath != LuaBehaviour.luaPath) {
+				// 尚未设置Lua根目录或找不到lua文件，显示文本框和设置按钮
+				string newLuaPath = EditorGUILayout.TextField(luaPath);
+				if (newLuaPath != luaPath) {
 					Undo.RecordObject(LuaBehaviour, "LuaPath");
 					LuaBehaviour.luaPath = newLuaPath;
 				}
@@ -111,6 +100,7 @@ namespace LuaApp {
 					SettingsService.OpenProjectSettings("Project/LuaBehaviour");
 				}
 			}
+			EditorGUI.EndDisabledGroup();
 			GUILayout.EndHorizontal();
 		}
 	}
