bce98d14b298dd1394edad3ccc0c446648bbf554
yun.wang <wy19910222@hotmail.com>
2024-01-16 10:17:59 +0800

    LUA_BEHAVIOUR_EXIST

---
+++ Assets/Tools/LuaBehaviour/Scripts/Editor/InitializeOnLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace LuaApp {
+	public class InitializeOnLoad : MonoBehaviour {
+		[InitializeOnLoadMethod]
+		private static void InitializeOnLoadMethod() {
+			const string SYMBOL_LUA_BEHAVIOUR_EXIST = "LUA_BEHAVIOUR_EXIST";
+			BuildTargetGroup group = BuildTargetGroup;
+			List<string> symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(group).Split(';').ToList();
+			if (!symbols.Contains(SYMBOL_LUA_BEHAVIOUR_EXIST)) {
+				symbols.Add(SYMBOL_LUA_BEHAVIOUR_EXIST);
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", symbols));
+			}
+		}
+		
+		private static BuildTargetGroup BuildTargetGroup =>
+#if UNITY_ANDROID
+				BuildTargetGroup.Android;
+#elif UNITY_IOS
+				BuildTargetGroup.iOS;
+#elif UNITY_STANDALONE
+				BuildTargetGroup.Standalone;
+#elif UNITY_WEBGL
+				BuildTargetGroup.WebGL;
+#endif
+	}
+}
